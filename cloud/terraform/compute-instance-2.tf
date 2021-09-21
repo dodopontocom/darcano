db688123826aea4e3e1fba9c5a21b3558e6e084d
Rodolfo Neto <rodolfotiago@gmail.com>
2021-09-21 20:56:18 -0300

    Create compute-instance-2.tf

---
+++ cloud/terraform/compute-instance-2.tf
@@ -0,0 +1,30 @@
+resource "google_compute_instance" "vm" {
+  provider     = google-beta
+  name         = "vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  network_interface {
+    network = "default"
+    access_config { }
+  }
+  tags = ["http-server", "https-server"]
+}
+
+resource "google_compute_machine_image" "image" {
+  provider        = google-beta
+  name            = "image-hello"
+  source_instance = google_compute_instance.vm.self_link
+}
+
+output "ip" {
+  value = google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
+}
