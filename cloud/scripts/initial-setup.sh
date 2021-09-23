b88b1e425750996f181264275ddb5f7fdf88cfe5
Rodolfo Neto <rodolfotiago@gmail.com>
2021-09-23 00:06:00 -0300

    [terraform-destroy]

--- cloud/scripts/initial-setup.sh
+++ cloud/scripts/initial-setup.sh
@@ -4,8 +4,8 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git jq bc make automake rsync htop curl build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ wget libncursesw5 libtool autoconf -y
 
-mkdir $HOME/git
-cd $HOME/git
+mkdir ~/git
+cd ~/git
 git clone https://github.com/input-output-hk/libsodium
 cd libsodium
 git checkout 66f017f1
