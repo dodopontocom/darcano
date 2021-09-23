53a76da753e8cd667717dbc6887056c4ff764c4e
dodopontocom <rodolfotiago@gmail.com>
2021-09-23 08:58:06 -0300

    terraform adjustments

--- cloud/terraform/instance_example.tf
+++ cloud/terraform/instance_example.tf
@@ -1,13 +1,11 @@
-resource "google_compute_instance" "vm" {
+resource "google_compute_instance" "base_vm" {
   provider     = google-beta
-  name         = "tf-vm2-${random_id.random_id.hex}"
+  name         = "tf-base-${random_id.random_id.hex}"
   machine_type = "e2-standard-2"
   zone         = "us-central1-a"
 
   boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
-    }
+    source      = google_compute_disk.base_disk.name
   }
 
   metadata_startup_script = "${file("${var.startup_script}")}"
@@ -19,14 +17,27 @@ resource "google_compute_instance" "vm" {
   tags = ["http-server", "https-server"]
 }
 
+data "google_compute_image" "ubuntu_image" {
+  family  = "ubuntu-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+resource "google_compute_disk" "base_disk" {
+  name  = "base-disk"
+  image = data.google_compute_image.ubuntu_image.self_link
+  size  = 40
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
 resource "google_compute_machine_image" "image" {
   provider        = google-beta
   name         = "tf-image-${random_id.random_id.hex}"
-  source_instance = google_compute_instance.vm.self_link
+  source_instance = google_compute_instance.base_vm.self_link
 }
 
-output "vm2-ip" {
-  value = google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
+output "base_vm-ip" {
+  value = google_compute_instance.base_vm.network_interface.0.access_config.0.nat_ip
 }
 
 
