b39bbad4845c3304942a84833c1b9c9d0dc0b00e
Rodolfo Neto <rodolfotiago@gmail.com>
2021-09-22 23:35:45 -0300

    [skip ci]

---
+++ cloud/scripts/initial-setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git jq bc make automake rsync htop curl build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ wget libncursesw5 libtool autoconf -y
+
+mkdir $HOME/git
+cd $HOME/git
+git clone https://github.com/input-output-hk/libsodium
+cd libsodium
+git checkout 66f017f1
+./autogen.sh
+./configure
+make
+sudo make install
+
+sudo ln -s /usr/local/lib/libsodium.so.23.3.0 /usr/lib/libsodium.so.23
+
+sudo apt-get -y install pkg-config libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev build-essential curl libgmp-dev libffi-dev libncurses-dev libtinfo5
+
+#curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
