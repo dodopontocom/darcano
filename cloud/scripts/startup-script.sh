ad4f551cb32a5c1d8b1261d9e276ca6ac43ec932
dodopontocom <rodolfotiago@gmail.com>
2021-10-27 16:41:21 -0300

    [terraform-apply] - daemon telegram bot fixed

--- cloud/scripts/startup-script.sh
+++ cloud/scripts/startup-script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # run at startup
+# 
 bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming/cloud/scripts/000-pre-step.sh)
 bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming/cloud/scripts/001-cardano-setup.sh)
 bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming/cloud/scripts/002-telegram-watcher.sh)
