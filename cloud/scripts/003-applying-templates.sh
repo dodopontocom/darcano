0edb8d12728a72e8bebaa779df7f8fe04bc1a9fc
dodopontocom <rodolfotiago@gmail.com>
2021-11-03 09:20:03 -0300

    [terraform-destroy] - fix template script

--- cloud/scripts/003-applying-templates.sh
+++ cloud/scripts/003-applying-templates.sh
@@ -14,7 +14,7 @@ NODE_CONFIG=testnet
 BP_IP=${NODE_HOME}/bp_ip
 RELAY_IP=${NODE_HOME}/relay_ip
 
-if [[ -f ${RELAY_IP} ]]; then
+if [[ ${RELAY_IP} ]]; then
     RN_NODE_EXTERNAL_IP=$(cat ${RELAY_IP})
     mv ${NODE_HOME}/${NODE_CONFIG}-topology.json ${NODE_HOME}/${NODE_CONFIG}-topology.json_bkp
     cat ${NODE_HOME}/${NODE_CONFIG}-topology.json_  | sed 's/RN_NODE_EXTERNAL_IP/'${RN_NODE_EXTERNAL_IP}/ > ${NODE_HOME}/${NODE_CONFIG}-topology.json
@@ -23,7 +23,7 @@ if [[ -f ${RELAY_IP} ]]; then
         curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage -d chat_id=${TELEGRAM_ID} -d text="${HOSTNAME} - Node could not restart..."
 fi
 
-if [[ -f ${BP_IP} ]]; then
+if [[ ${BP_IP} ]]; then
     BP_NODE_INTERNAL_IP=$(cat ${BP_IP})
     mv ${NODE_HOME}/${NODE_CONFIG}-topology.json ${NODE_HOME}/${NODE_CONFIG}-topology.json_bkp
     cat ${NODE_HOME}/${NODE_CONFIG}-topology.json_  | sed 's/BP_NODE_INTERNAL_IP/'${BP_NODE_INTERNAL_IP}/ > ${NODE_HOME}/${NODE_CONFIG}-topology.json
