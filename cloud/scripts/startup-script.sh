1686ae9d85aaa2ddd30d5ea325b4706a5e1c9e8d
dodopontocom <rodolfotiago@gmail.com>
2021-11-04 16:36:19 -0300

    [terraform-destroy] - reboot bot tests

--- cloud/scripts/startup-script.sh
+++ cloud/scripts/startup-script.sh
@@ -7,3 +7,5 @@ echo ${HOSTNAME} | grep relaynode && \
 echo ${HOSTNAME} | grep blockproducer && \
     bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/telegram-tests/cloud/scripts/002-01-darcano.sh)
 #bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.1/cloud/scripts/003-applying-templates.sh)
+
+#
