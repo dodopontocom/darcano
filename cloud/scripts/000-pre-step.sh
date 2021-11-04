6aabc4e548fd70a58e1b71e60447b47c314e04f1
dodopontocom <rodolfotiago@gmail.com>
2021-11-04 15:57:45 -0300

    [terraform-apply] darlene1 and darcano in systemd fixed

--- cloud/scripts/000-pre-step.sh
+++ cloud/scripts/000-pre-step.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#
+
 DARLENE1_TOKEN=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/attributes/DARLENE1_TOKEN)
 TELEGRAM_ID=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/attributes/TELEGRAM_ID)
 curl -s -X POST https://api.telegram.org/bot${DARLENE1_TOKEN}/sendMessage -d chat_id=${TELEGRAM_ID} -d text="Hello from ${HOSTNAME}"
