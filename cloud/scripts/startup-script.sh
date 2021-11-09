2547b3c0c47b28dd4eacd6a509aee50ea91b671d
dodopontocom <rodolfotiago@gmail.com>
2021-11-09 14:51:44 -0300

    [terraform-apply] - looks all good

--- cloud/scripts/startup-script.sh
+++ cloud/scripts/startup-script.sh
@@ -6,8 +6,7 @@ echo ${HOSTNAME} | grep relaynode && \
     bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/002-00-darlene1.sh)
 echo ${HOSTNAME} | grep blockproducer && \
     bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/002-01-darcano.sh)
-#bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/003-applying-templates.sh)
 echo ${HOSTNAME} | grep relaynode && \
-    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/004-registering-pool.sh)
+    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/003-registering-pool.sh)
 echo ${HOSTNAME} | grep relaynode && \    
-    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/005-topology-updater.sh)
+    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/004-topology-updater.sh)
