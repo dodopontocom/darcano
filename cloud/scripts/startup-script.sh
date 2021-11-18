c30d143bd0eb80461eb5dd6b94603f1ba660eaeb
dodopontocom <rodolfotiago@gmail.com>
2021-11-18 10:54:29 -0300

    [terraform-destroy] - fix firewall first

--- cloud/scripts/startup-script.sh
+++ cloud/scripts/startup-script.sh
@@ -2,15 +2,15 @@
 
 # this is the startup script sequencer
 
-bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/000-pre-step.sh)
-bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/001-cardano-setup.sh)
+bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/feature/tbot-v2/cloud/scripts/000-pre-step.sh)
+bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/feature/tbot-v2/cloud/scripts/001-cardano-setup.sh)
 if [[ $(echo ${HOSTNAME} | grep relaynode) ]]; then
-    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/002-00-darlene1.sh)
+    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/feature/tbot-v2/cloud/scripts/002-00-darlene1.sh)
 fi
 if [[ $(echo ${HOSTNAME} | grep blockproducer) ]]; then
-    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/002-01-darcano.sh)
+    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/feature/tbot-v2/cloud/scripts/002-01-darcano.sh)
 fi
 if [[ $(echo ${HOSTNAME} | grep relaynode) ]]; then
-    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/003-registering-pool.sh)
-    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/terraforming-v0.6/cloud/scripts/004-topology-updater.sh)
+    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/feature/tbot-v2/cloud/scripts/003-registering-pool.sh)
+    bash <(curl -s https://raw.githubusercontent.com/dodopontocom/darcano/feature/tbot-v2/cloud/scripts/004-topology-updater.sh)
 fi
