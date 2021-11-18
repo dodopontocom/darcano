917fb8ac6f57bc3465a3a2108265cfbbb51b087a
dodopontocom <rodolfotiago@gmail.com>
2021-11-18 10:41:32 -0300

    [terraform-destroy] - fix firewall first

--- cloud/scripts/000-pre-step.sh
+++ cloud/scripts/000-pre-step.sh
@@ -3,14 +3,13 @@
 # runs on both BP&R
 # update and upgrade apt packages
 
-
 DARLENE1_TOKEN=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/attributes/DARLENE1_TOKEN)
 TELEGRAM_ID=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/attributes/TELEGRAM_ID)
 curl -s -X POST https://api.telegram.org/bot${DARLENE1_TOKEN}/sendMessage -d chat_id=${TELEGRAM_ID} -d text="Hello from ${HOSTNAME}"
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y git jq bc make automake rsync htop curl \
+sudo apt-get install -y git jq bc make automake rsync htop \
     build-essential pkg-config libffi-dev libgmp-dev \
     libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev \
     make g++ wget libncursesw5 libtool autoconf libncurses-dev libtinfo5
